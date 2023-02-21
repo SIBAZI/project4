ef2b6ae6e2bcfa8dd0aec7a773384ed9548ca51f
sibazi <yuzu.taichi.shiba@gmail.com>
2023-02-22 00:02:28 +0900

    2/22

--- main.js
+++ main.js
@@ -116,9 +116,11 @@
       }else{
        navi1.classList.remove('open1');
        main.classList.remove('slide');
+
       }
 
       detail1.classList.remove('detail1denote1');
+      detail1_2.classList.remove('detail1-2denote1-2');
 
       navi2.classList.remove('open2');
       navi3.classList.remove('open3');
@@ -144,6 +146,7 @@
       }
 
       detail2.classList.remove('detail2denote2');
+      detail2_2.classList.remove('detail2-2denote2-2');
       
       navi1.classList.remove('open1');
       navi3.classList.remove('open3');
@@ -169,6 +172,7 @@
       }
 
       detail3.classList.remove('detail3denote3');
+      detail3_2.classList.remove('detail3-2denote3-2');
 
       navi1.classList.remove('open1');
       navi2.classList.remove('open2');
@@ -194,6 +198,7 @@
       }
 
       detail4.classList.remove('detail4denote4');
+      detail4_2.classList.remove('detail4-2denote4-2');
 
       navi1.classList.remove('open1');
       navi2.classList.remove('open2');
@@ -219,6 +224,7 @@
       }
 
       detail5.classList.remove('detail5denote5');
+      detail5_2.classList.remove('detail5-2denote5-2');
 
       navi1.classList.remove('open1');
       navi2.classList.remove('open2');
@@ -244,6 +250,7 @@
       }
 
       detail6.classList.remove('detail6denote6');
+      detail6_2.classList.remove('detail6-2denote6-2');
 
       navi1.classList.remove('open1');
       navi2.classList.remove('open2');
@@ -269,6 +276,7 @@
       }
 
       detail7.classList.remove('detail7denote7');
+      detail7_2.classList.remove('detail7-2denote7-2');
 
       navi1.classList.remove('open1');
       navi2.classList.remove('open2');
@@ -294,6 +302,7 @@
       }
 
       detail8.classList.remove('detail8denote8');
+      detail8_2.classList.remove('detail8-2denote8-2');
 
       navi1.classList.remove('open1');
       navi2.classList.remove('open2');
@@ -319,6 +328,7 @@
       }
 
       detail9.classList.remove('detail9denote9');
+      detail9_2.classList.remove('detail9-2denote9-2');
 
       navi1.classList.remove('open1');
       navi2.classList.remove('open2');
@@ -344,6 +354,7 @@
       }
 
       detail10.classList.remove('detail10denote10');
+      detail10_2.classList.remove('detail10-2denote10-2');
 
       navi1.classList.remove('open1');
       navi2.classList.remove('open2');
@@ -368,6 +379,9 @@
      }else{
       detail1.classList.remove('detail1denote1');
      }
+
+     detail1_2.classList.remove('detail1-2denote1-2');
+
     }
 
     cataloguemenu2.onclick = function(){
@@ -376,6 +390,9 @@
      }else{
       detail2.classList.remove('detail2denote2');
      }
+
+     detail2_2.classList.remove('detail2-2denote2-2');
+
     }
 
     cataloguemenu3.onclick = function(){
@@ -384,6 +401,9 @@
      }else{
       detail3.classList.remove('detail3denote3');
      }
+
+     detail3_2.classList.remove('detail3-2denote3-2');
+
     }
 
     cataloguemenu4.onclick = function(){
@@ -392,6 +412,9 @@
      }else{
       detail4.classList.remove('detail4denote4');
      }
+
+     detail4_2.classList.remove('detail4-2denote4-2');
+
     }
 
     cataloguemenu5.onclick = function(){
@@ -400,6 +423,9 @@
      }else{
       detail5.classList.remove('detail5denote5');
      }
+
+     detail5_2.classList.remove('detail5-2denote5-2');
+
     }
 
     cataloguemenu6.onclick = function(){
@@ -408,6 +434,9 @@
      }else{
       detail6.classList.remove('detail6denote6');
      }
+
+     detail6_2.classList.remove('detail6-2denote6-2');
+
     }
 
     cataloguemenu7.onclick = function(){
@@ -416,6 +445,9 @@
      }else{
       detail7.classList.remove('detail7denote7');
      }
+
+     detail7_2.classList.remove('detail7-2denote7-2');
+
     }
 
     cataloguemenu8.onclick = function(){
@@ -424,6 +456,9 @@
      }else{
       detail8.classList.remove('detail8denote8');
      }
+
+     detail8_2.classList.remove('detail8-2denote8-2');
+
     }
 
     cataloguemenu9.onclick = function(){
@@ -432,6 +467,9 @@
      }else{
       detail9.classList.remove('detail9denote9');
      }
+
+     detail9_2.classList.remove('detail9-2denote9-2');
+
     }
 
     cataloguemenu10.onclick = function(){
@@ -440,10 +478,133 @@
      }else{
       detail10.classList.remove('detail10denote10');
      }
+
+     detail10_2.classList.remove('detail10-2denote10-2');
+
     }
 
+    let cataloguemenu1_2 =document.querySelector('.cataloguemenu1-2');
+    let cataloguemenu2_2 =document.querySelector('.cataloguemenu2-2');
+    let cataloguemenu3_2 =document.querySelector('.cataloguemenu3-2');
+    let cataloguemenu4_2 =document.querySelector('.cataloguemenu4-2');
+    let cataloguemenu5_2 =document.querySelector('.cataloguemenu5-2');
+    let cataloguemenu6_2 =document.querySelector('.cataloguemenu6-2');
+    let cataloguemenu7_2 =document.querySelector('.cataloguemenu7-2');
+    let cataloguemenu8_2 =document.querySelector('.cataloguemenu8-2');
+    let cataloguemenu9_2 =document.querySelector('.cataloguemenu9-2');
+    let cataloguemenu10_2 =document.querySelector('.cataloguemenu10-2');
 
+    let detail1_2 = document.querySelector('.detail1-2');
+    let detail2_2 = document.querySelector('.detail2-2');
+    let detail3_2 = document.querySelector('.detail3-2');
+    let detail4_2 = document.querySelector('.detail4-2');
+    let detail5_2 = document.querySelector('.detail5-2');
+    let detail6_2 = document.querySelector('.detail6-2');
+    let detail7_2 = document.querySelector('.detail7-2');
+    let detail8_2 = document.querySelector('.detail8-2');
+    let detail9_2 = document.querySelector('.detail9-2');
+    let detail10_2 = document.querySelector('.detail10-2');
 
+    cataloguemenu1_2.onclick = function(){
+      if(!detail1_2.classList.contains('detail1-2denote1-2')){
+        detail1_2.classList.add('detail1-2denote1-2');
+      }else{
+        detail1_2.classList.remove('detail1-2denote1-2');
+      }
+
+      detail1.classList.remove('detail1denote1');
+
+    }
+    cataloguemenu2_2.onclick = function(){
+      if(!detail2_2.classList.contains('detail2-2denote2-2')){
+        detail2_2.classList.add('detail2-2denote2-2');
+      }else{
+        detail2_2.classList.remove('detail2-2denote2-2');
+      }
+
+      detail2.classList.remove('detail2denote2');
+
+    }
+    cataloguemenu3_2.onclick = function(){
+      if(!detail3_2.classList.contains('detail3-2denote3-2')){
+        detail3_2.classList.add('detail3-2denote3-2');
+      }else{
+        detail3_2.classList.remove('detail3-2denote3-2');
+      }
+
+      detail3.classList.remove('detail3denote3');
+
+    }
+    cataloguemenu4_2.onclick = function(){
+      if(!detail4_2.classList.contains('detail4-2denote4-2')){
+        detail4_2.classList.add('detail4-2denote4-2');
+      }else{
+        detail4_2.classList.remove('detail4-2denote4-2');
+      }
+
+      detail4.classList.remove('detail4denote4');
+
+    }
+    cataloguemenu5_2.onclick = function(){
+      if(!detail5_2.classList.contains('detail5-2denote5-2')){
+        detail5_2.classList.add('detail5-2denote5-2');
+      }else{
+        detail5_2.classList.remove('detail5-2denote5-2');
+      }
+
+      detail5.classList.remove('detail5denote5');
+
+    }
+    cataloguemenu6_2.onclick = function(){
+      if(!detail6_2.classList.contains('detail6-2denote6-2')){
+        detail6_2.classList.add('detail6-2denote6-2');
+      }else{
+        detail6_2.classList.remove('detail6-2denote6-2');
+      }
+
+      detail6.classList.remove('detail6denote6');
+
+    }
+    cataloguemenu7_2.onclick = function(){
+      if(!detail7_2.classList.contains('detail7-2denote7-2')){
+        detail7_2.classList.add('detail7-2denote7-2');
+      }else{
+        detail7_2.classList.remove('detail7-2denote7-2');
+      }
+
+      detail7.classList.remove('detail7denote7');
+
+    }
+    cataloguemenu8_2.onclick = function(){
+      if(!detail8_2.classList.contains('detail8-2denote8-2')){
+        detail8_2.classList.add('detail8-2denote8-2');
+      }else{
+        detail8_2.classList.remove('detail8-2denote8-2');
+      }
+
+      detail8.classList.remove('detail8denote8');
+
+    }
+    cataloguemenu9_2.onclick = function(){
+      if(!detail9_2.classList.contains('detail9-2denote9-2')){
+        detail9_2.classList.add('detail9-2denote9-2');
+      }else{
+        detail9_2.classList.remove('detail9-2denote9-2');
+      }
+
+      detail9.classList.remove('detail9denote9');
+
+    }
+    cataloguemenu10_2.onclick = function(){
+      if(!detail10_2.classList.contains('detail10-2denote10-2')){
+        detail10_2.classList.add('detail10-2denote10-2');
+      }else{
+        detail10_2.classList.remove('detail10-2denote10-2');
+      }
+
+      detail10.classList.remove('detail10denote10');
+
+    }
 
 
 
